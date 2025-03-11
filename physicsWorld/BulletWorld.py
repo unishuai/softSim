74664eaa27768a57cce0373c04e32b43c04c873a
unishuai <unishuai@gmail>
2025-03-11 23:13:34 +0800

    更新了文件路径,修改为通过主函数启动

--- physicsWorld/BulletWorld.py
+++ physicsWorld/BulletWorld.py
@@ -41,9 +41,9 @@ class BulletWorld(QObject):
         self.tableId = self.p.loadURDF("table/table.urdf", basePosition=[0, -0.7, 0])
         self.deskId = self.p.loadURDF("table_square/table_square.urdf", basePosition=[0, 0, 0],globalScaling=0.5)
 
-        self.cupId = self.p.loadURDF("../otherModel/urdf/coffee_cup.urdf", np.array([0.3, -0.6, 1]), globalScaling=1)
+        self.cupId = self.p.loadURDF("otherModel/urdf/coffee_cup.urdf", np.array([0.3, -0.6, 1]), globalScaling=1)
         #用来创建那个物体
-        cupBodyData=TactoBodyDataClass.TactoBodyDataClass("../otherModel/urdf/coffee_cup.urdf", np.array([0.3, -0.6, 1]), 1,self.cupId)
+        cupBodyData=TactoBodyDataClass.TactoBodyDataClass("otherModel/urdf/coffee_cup.urdf", np.array([0.3, -0.6, 1]), 1,self.cupId)
 
         self.robot = mySim.Ur10FreeHnadSimAuto(self.p, [0, 0, 0.3])
         self.handClosePose =self.robot.getHandClosedPose()
